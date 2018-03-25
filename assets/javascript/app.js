0d45ed70891d412c6c70bc5bb970c2b0a375b39e
Andrea Velardi <>
2018-03-25 11:07:33 -0500

    changes 3.25.2018v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //Global Variables
 
-var time = 20;
+var time = 11;
 var questions = 
 {
     q1: ['Before the Foo Fighters, David Grohl was the lead drummer in which famous rock band?'],
@@ -23,7 +23,7 @@ var intervalidId;
 //Functions
 
 //test
-console.log(questions.q2); 
+console.log(questions.q1); 
 
 
 function startGame() 
@@ -33,33 +33,44 @@ function startGame()
 
 function gamePlay() 
 {
-    $('.startBtn').on('click', function () 
+    $('.startBtn').on('click', function () //by clicking on the start button it calls the quest1 function and the timeCount function 
     {
         //removes all buttons with the class startbtn
         $('button').remove('.startBtn');
-        quest1();
+        timeCount();
+        quest1(); 
     });
+}
+function timeCount()
+{
+    time = 11; // did 11 so it would reflect 10 seconds on the DOM
+    interval = setInterval(timeRemaining,1000); // one second internal
+    $('.btnContainer').append('<div class="timeRem">' 
+                + 'Time Remaining: ' + //this text will appear on DOM
+                '<div class="timerCountDown"></div>' +
+                '</div>' +
+                '<div class="quest"></div');//create div for the questions
+}
+// print the time remaining to the Dom and once it is = 0 then it stops    
+function timeRemaining()
+{
+    //decrements the time by 1 second
+    time--; 
+    //prints the remaining time 
+    $('.timerCountDown').text(time); //adding the text of time within timerCountDown div on DOM
+    //test
+    console.log(time); 
+    //if the time reaches 0 then cancel the time and start over
+    if(time === 0)
+    {
+        console.log(true); 
+        //stops the time
+        clearInterval(interval); 
+    } 
 }
 function quest1()
 {
-    var newDiv = $('<div class=" quest q1">'); 
-    newDiv.html(questions.q2); 
-
-    $('.btnContainer').append(newDiv); 
-    intervalidId = setInterval(timeCount,1000); 
-}
-    
-function timeCount(){
-    time--; 
-    //test
-    console.log(time); 
-    //this clears and stops the time
-    if(time === 0)
-    {
-    clearInterval(intervalidId); 
-    //debugging
-    console.log(true); 
-    }
+    $('.quest').append(questions.q1);//append the data from the questions.q1 to the quest div on the DOM
 }
 
 gamePlay(); 
